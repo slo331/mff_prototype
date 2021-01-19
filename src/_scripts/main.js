4a1c58d1457e3e0882cd48e377dd0d733c865efc
Stephen Orioste <stephen.orioste@sg.ey.com>
2021-01-19 19:57:27 +0800

    Created Navigation json, updated base template and homepage page - added all modules, mixin and variables in js and css files

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -1,8 +1,36 @@
 // Main javascript entry point
 // Should handle bootstrapping/starting application
 'use strict';
-import Button from '../_modules/button/button';
+
+// Import Modules
+import ScrollTop from '../_modules/atoms/scroll-top/scroll-top';
+import SiteNav from '../_modules/molecules/site-nav/site-nav';
+import MegaMenu from '../_modules/molecules/mega-menu/mega-menu';
+import SiteSearch from '../_modules/molecules/site-search/site-search';
+
 $(() => {
-  new Button();
+  let config = {
+		breakpoints: {
+			tablet: 768,
+			desktop: 1024,
+			lgDesktop: 1280
+		}
+  };
+
+  if($('.nav-wrap').length) {
+    new SiteNav(config);
+  }
+
+  if($('.site-search').length) {
+    new SiteSearch();
+  }
+
+  if($('.mega-menu').length) {
+    new MegaMenu();
+  }
+
+  if($('.scroll-top').length) {
+    new ScrollTop();
+  }
 });
 
