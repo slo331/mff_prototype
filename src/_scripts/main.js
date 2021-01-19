9574f70824a1d2869e4bf6994feca5a24c4a02b0
Stephen Orioste <stephen.orioste@sg.ey.com>
2021-01-20 00:07:55 +0800

    Homepage carousel markup and layout updates

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -7,6 +7,7 @@ import ScrollTop from '../_modules/atoms/scroll-top/scroll-top';
 import SiteNav from '../_modules/molecules/site-nav/site-nav';
 import MegaMenu from '../_modules/molecules/mega-menu/mega-menu';
 import SiteSearch from '../_modules/molecules/site-search/site-search';
+import HomeCarousel from '../_modules/organisms/home-carousel/home-carousel';
 
 $(() => {
   let config = {
@@ -31,6 +32,10 @@ $(() => {
 
   if($('.scroll-top').length) {
     new ScrollTop();
+  }
+
+  if($('.carousel-home').length) {
+    new HomeCarousel();
   }
 });
 
