fe9333064259c467bb87d4b56a0a291a261a1b0b
Stephen Orioste <stephen.orioste@sg.ey.com>
2021-01-19 19:48:57 +0800

    Created Yolo generated site with webpack

---
+++ src/_scripts/main.js
@@ -0,0 +1,8 @@
+// Main javascript entry point
+// Should handle bootstrapping/starting application
+'use strict';
+import Button from '../_modules/button/button';
+$(() => {
+  new Button();
+});
+
