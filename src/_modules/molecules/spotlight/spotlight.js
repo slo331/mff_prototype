f819b8becf60d222f9e7f5f4c814f596c1a6a5d8
Stephen Orioste <stephen.orioste@sg.ey.com>
2021-01-23 20:16:39 +0800

    Created Title and Spotlight module

---
+++ src/_modules/molecules/spotlight/spotlight.js
@@ -0,0 +1,8 @@
+'use strict';
+
+export default class Spotlight {
+  constructor() {
+    this.name = 'Spotlight';
+    console.log('Spotlight');
+  }
+}
---
+++ src/_modules/molecules/title/title.js
@@ -0,0 +1,8 @@
+'use strict';
+
+export default class Title {
+  constructor() {
+    this.name = 'Title';
+    console.log('Title');
+  }
+}
