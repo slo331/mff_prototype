4497818f262bf836ca79bf2329b77d476653738b
Stephen Orioste <stephen.orioste@sg.ey.com>
2021-02-02 09:05:17 +0800

    Fixed carousel dots and arrows aligment in homepage

--- src/_modules/atoms/carousel/carousel.js
+++ src/_modules/atoms/carousel/carousel.js
@@ -12,23 +12,43 @@ export default class Carousel {
       $numSlick++;
       let $el = $(el);
       let $wrap = $el.children('.carousel-wrap');
-      let $carousel = $wrap.addClass('slider-' + $numSlick);
-      
-      $carousel.slick({
-        speed: 800,
-        infinite: true,
-        autoplay: false,
-        fade: false,
-        draggable: true,
-        pauseOnDotsHover: true,
-        dots: true,
-        arrows: true,
-        mobileFirst: true,
-        adaptiveHeight: true,
-        appendDots: $('.carousel-pagination .dots', $el),
-        appendArrows: $('.carousel-pagination', $el),
-        prevArrow: $('.carousel-pagination .prev', $el),
-        nextArrow: $('.carousel-pagination .next', $el)
+      let $carouselWrap = $wrap.addClass('slider-' + $numSlick);
+
+      let $galleryHeight = () => {
+        let $carouselItem = $('.carousel-item', $carouselWrap);
+        let $gallery = $('.gallery', $carouselItem);
+
+        $('.item-desc', $gallery).matchHeight({
+          byRow: false
+        });
+
+        let $descHeight = $('.item-desc', $gallery).outerHeight();
+        let $imgHeight = $('.item-img', $gallery).outerHeight();
+
+        $('.item-img', $gallery).css({
+          height: $imgHeight - $descHeight
+        });
+
+      }
+
+      $.when($galleryHeight())
+      .done(() => {
+        $carouselWrap.slick({
+          speed: 800,
+          infinite: true,
+          autoplay: false,
+          fade: false,
+          draggable: true,
+          pauseOnDotsHover: true,
+          dots: true,
+          arrows: true,
+          mobileFirst: true,
+          adaptiveHeight: true,
+          appendDots: $('.carousel-pagination .dots', $el),
+          appendArrows: $('.carousel-pagination', $el),
+          prevArrow: $('.carousel-pagination .prev', $el),
+          nextArrow: $('.carousel-pagination .next', $el)
+        });
       });
     });
   }
